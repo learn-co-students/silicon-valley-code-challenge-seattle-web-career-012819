1cff5abb01e0ceb1407064affe073978812da906
ScottDenton <scottyd_14@hotmail.com>
2019-02-11 09:03:14 -0800

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,34 @@
 class Startup
 
+  @@all = []
+
+  attr_reader :founder, :domain
+
+  attr_accessor :name
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # how can you change the name and domain when your are explicity told not too, without creating a whole new startup
+  def pivot(domain, name)
+    Startup.new(name, self.founder, self.domain)
+  end
+
+  def self.find_by_founder(founder)
+    self.all.find{|startup| startup.founder == founder}
+  end
+
+  def self.domains
+    self.all.map{|startup| startup.domain}
+  end
+
 end
